3ea63f0dceb25fd5ca9b4afb2cf71b0ad69fe4ab
Jay Smith <jaysmith024@gmail.com>
2016-03-29 08:42:08 -0400

    re uploading new files

--- web/src/app/Labels.js
+++
@@ -1,9 +0,0 @@
-/**
- * Global application strings and labels
- */
- namespace("app.labels");
-
-app.labels.PLEASE_WAIT 	= "Fetching...";
-app.labels.SHOWING_ALL 	= "SHOWING ALL";
-app.labels.SHOWING 		= "SHOWING";
-app.labels.RESULTS_TITLE= "Videos Found";
--- web/src/app/Session.js
+++
@@ -1,5 +0,0 @@
-Session={};
-Session.State = {};
-Session.State.AccountProfile={};
-Session.State.CurrentProfile={};
-
